e485f0d060a6e0c330891d6c0b507a4d8c85fc5c
Daniel Ruiz <danielruiz2009@hotmail.com>
2024-11-13 12:54:26 -0500

    Working with enemies

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public enum GhostNodeStateEnum
+    public enum EnemyNodeStateEnum
     {
         respawning,
         leftNode,
@@ -13,9 +13,39 @@ public class EnemyController : MonoBehaviour
         startNode,
         movingInNodes
     }
-    void Start()
-    {
 
+    public enum EnemyType
+    {
+        mantis,
+        beetle
+    }
+
+    public EnemyNodeStateEnum enemyNodeState;
+    public EnemyType enemyType;
+
+    public GameObject enemyNodeLeft;
+    public GameObject enemyNodeRight;
+    public GameObject enemyNodeCenter;
+    public GameObject enemyNodeStart;
+
+    public MovementController movementController;
+
+    public GameObject startingNode;
+
+    void Awake()
+    {
+        movementController = GetComponent<MovementController>();
+        startingNode = enemyNodeStart;
+
+        if (enemyType == EnemyType.mantis)
+        {
+            enemyNodeState = EnemyNodeStateEnum.centerNode;
+        }
+        else if (enemyType == EnemyType.beetle)
+        {
+            enemyNodeState = EnemyNodeStateEnum.startNode;
+            startingNode = enemyNodeStart;
+        }
     }
 
     // Update is called once per frame
